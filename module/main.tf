0e487d1e43b82ab4dc477f45e4996cd549a9f293
Sahithya Priya <sahithya3098@gmail.com>
2025-04-21 11:44:25 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -5,9 +5,7 @@ provider "aws"{
 resource "aws_instance" "example"{
     ami = var.ami_id
     instance_type = var.instance_type
-    subnet_id = var.subnet_id
     key_name = var.key_name
-    vpc_security_group_ids = var.security_group_ids
 
 
     tags = {
